c5d4f45b346af1d9faf7a9a61222c7339e916b42
Suman Chandra Barman <sumanchandrabarman999@gmail.com>
2025-09-13 12:40:20 +0600

    Update sidebar width

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,7 +4,13 @@ import React from "react";
 
 function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider>
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "14rem", 
+        } as React.CSSProperties
+      }
+    >
       <DashboardSidebar />
       <main className="relative w-full">
         <div className="absolute top-[30px] md:left-[-13px] z-10">
